d7efeb31701ce66ce39200fa8392ef6b312d0509
camiloandrespinto <camiloapovalle@hotmail.com>
2016-04-11 20:30:57 -0500

    Modificacion modelo asistencia

--- Code/Laravel/app/Asistencia.php
+++ Code/Laravel/app/Asistencia.php
@@ -8,4 +8,10 @@ class Asistencia extends Model
 {
     protected $table = 'asistencia';
     protected $fillable = ['id_profesor', 'id_evento', 'id_estudiante'];
+        /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id_asistencia';   
 }
