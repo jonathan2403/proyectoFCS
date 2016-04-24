ed9ef97e405598b80dcea3fe724f5ea0e6ea6310
jonathan <jonathancuellarp@gmail.com>
2016-04-24 15:11:43 -0500

    acomodar index

--- Code/Laravel/resources/views/componentes/publicacion_investigacion/index.blade.php
+++ Code/Laravel/resources/views/componentes/publicacion_investigacion/index.blade.php
@@ -1,17 +1,17 @@
 @extends('layaouts.tablas')
 @section('content')
 	<section class="content">
-   	
-			<div class="col-xs-11">
+
+		<div class="row">
+			<div class="col-xs-12">
 				<div class="box">
 					<div class="box-header">
 	                  @include('layaouts.partials.mensaje')
-						<h3 class="box-title">Listado de publicaciones</h3>
 					</div><!-- /.box-header -->
 					<div class="box-body">
 						<div class="row form-group">
 							<div class="col-md-3">
-								<a href="{!! URL('publicacion-investigacion/create') !!}" class="btn btn-success"><i class="fa fa-plus"></i> Nuevo</a>
+								<a href="{!! URL('joven-investigador/create') !!}" class="btn btn-success"><i class="fa fa-plus"></i> Nuevo</a>
 							</div>
 						</div>
 							<table id="example3" class="table table-bordered table-striped">
@@ -48,19 +48,8 @@
 							</table>
 					</div><!-- /.box-body -->
 				</div><!-- /.box -->
-				<button type="button" class="btn" onClick ="$('#example3').tableExport({type:'pdf',pdfFontSize:'7',escape:'false'});"><span class="glyphicon glyphicon-download"></span> PDF</button>|<button id="button-excel" class="btn"><span class="glyphicon glyphicon-download"></span> Excel</button>
 			</div><!-- /.col -->
-
 		</div><!-- /.row -->
+		<button type="button" class="btn" onClick ="$('#example3').tableExport({type:'pdf',pdfFontSize:'7',escape:'false'});"><span class="glyphicon glyphicon-download"></span> PDF</button>|<button id="button-excel" class="btn"><span class="glyphicon glyphicon-download"></span> Excel</button>
 	</section><!-- /.content -->
-@endsection
-@section('scripts')
-  <script type="text/javascript">
-  $(document).ready(function () {
-           $("#button-excel").click(function(e) {
-          window.open('data:application/vnd.openxmlformats-officedocument.spreadsheetml.sheet,' + encodeURIComponent($('#dvData').html()));
-        e.preventDefault();
-        });
-    });
-  </script>
 @endsection
