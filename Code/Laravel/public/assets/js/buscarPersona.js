58ceba9a81a9a5c9a58fc8cdcfdfc8db6340b35d
jonathan <jonathancuellarp@gmail.com>
2016-05-01 12:00:24 -0500

    modificacion autocompletado

--- Code/Laravel/public/assets/js/buscarPersona.js
+++ Code/Laravel/public/assets/js/buscarPersona.js
@@ -1,23 +1,32 @@
-/* autor: Andrea Camargo
+/* autor: Jonathan Cuellar
 * Autocompletar nommbres, apellidos y cedula de usuarios distribuidores y supervisores
 * */
-//var input_oculto= $("#personaPasarSaldo_escondido");
-var input_visible=$("#nombre_estudiante");
+var idestudiante_oculto = $("#id_estudiante");
+var input_visible = $("#nombre_estudiante");
 var boton=$("#consignarsaldo");
 
     boton.hide();
     $("#nombre_estudiante").autocomplete({
         source: function(request, response)
         {
-            /*input_visible.change(function(){
-                boton.hide();
+            input_visible.change(function(){
+                //boton.hide();
                 $("#personaPasarSaldo-1").text("No ha seleccionado un usuario correcto");
-            });*/
+            });
             $.getJSON("http://localhost:8000/buscarEstudiante/"+request.term,{
-                //          term:  ( request.term )
             },response);//fin get JSON
         }, /* este es el script que realiza la busqueda */
-        //minLength: 2, /* le decimos que espere hasta que haya 2 caracteres escritos */
+
+        // seleccionar id
+        select:function(event, ui)
+        {
+
+            input_visible.val(ui.item.primer_nombre+" "+ui.item.segundo_nombre+" "+ui.item.apellido_paterno+" "+ui.item.apellido_materno);
+            $("#personaPasarSaldo-1").text("Ha seleccionado "+ui.item.primer_nombre+" "+ui.item.segundo_nombre+" "+ui.item.apellido_paterno+" "+ui.item.apellido_materno);
+            idestudiante_oculto.val(ui.item.id);
+            //boton.show();
+            return false;
+        }
     }).autocomplete( "instance" )._renderItem = function( ul, item ) {
         return $( "<li class='autocompletar_saldos'>"  )
             .append(
--- Code/Laravel/resources/views/componentes/joven_investigador/partials/form.blade.php
+++ Code/Laravel/resources/views/componentes/joven_investigador/partials/form.blade.php
@@ -1,7 +1,7 @@
       
-   {!! Form::text('id_estudiante', null,['class' => 'form-control', 'id' => 'nombre_estudiante','placeholder'=>'Nombre/cedula']) !!}
-                        
-   {!! Form::hidden('personaPasarSaldo_escondido', null, array('id' =>'personaPasarSaldo_escondido')) !!}
+   {!! Form::text('estudiante', null,['class' => 'form-control', 'id' => 'nombre_estudiante','placeholder'=>'Nombre/cedula']) !!}
+   <div id="personaPasarSaldo-1"></div>                     
+   {!! Form::hidden('id_estudiante', null, ['id' => 'id_estudiante']) !!}
 </br>                    
                     
                         
